43d4131264b7b312ce87bc2a868b5c84622b740c
mtcodingtech <mtcodingtech@gmail.com>
2025-07-01 00:12:35 +0630

    day1 js

---
+++ JS/Day1/data-types.js
@@ -0,0 +1,40 @@
+// let age = 20.33;
+// console.log(age, typeof age)
+
+// ******************************
+
+// let username = `john`;
+// console.log(username, typeof username)
+
+// ********************************
+// let isGreater = 10 > 20;
+// console.log(isGreater, typeof isGreater)
+
+// ********************************
+
+// let username = null;
+// console.log(username, typeof username)
+
+// let username2;
+// console.log(username2, typeof username2)
+
+// console.log(username == username2) // value
+// console.log(username === username2) // value & data type
+
+// *****************************************
+
+// let fruits = ["apple", "orange", "mango"];
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+
+// ******************************************
+
+let user = {
+  name: "John",
+  age: 20,
+};
+
+console.log(user.name, user.age);
+console.log(user["name"], user["age"]);
---
+++ JS/Day1/variables.js
@@ -0,0 +1,31 @@
+// var username = "John";
+// username = "Rose";
+
+
+// let age = 20;
+// age = 30;
+
+// const city = "ygn";
+// city = "mdy"; // error
+
+
+// console.log(username, age, city)
+
+// *************************************
+
+
+// var username = "John";
+
+// if(true){
+//     let username = "Rose";
+//     console.log(username)
+// }
+
+// console.log(username)
+
+// ************************************
+
+"use strict";
+// num = 5; // error
+let num = 5;
+console.log(num)
