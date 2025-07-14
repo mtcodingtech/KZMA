c1b663eb9942d24ab2e845f357bcdea05c75c07a
mtcodingtech <mtcodingtech@gmail.com>
2025-07-14 10:49:29 +0630

    js final

---
+++ JS/Day4/array-method.js
@@ -0,0 +1,11 @@
+let fruits = ["apple", "mango", "orange", "kiwi"];
+
+// let result1 = fruits.forEach((fruit) => fruit);
+// let result2 = fruits.map((fruit) => fruit);
+// console.log(result1)
+// console.log(result2)
+
+// let result = fruits.filter((fruit) => fruit.length > 4);
+let result = fruits.find((fruit) => fruit.length > 4);
+
+console.log(result)
